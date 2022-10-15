c9c2bf4b55a354610ef4db81c6cd5df5d4f881ea
Saif <saifobeidat@gmail.com>
2022-10-15 11:50:12 +0300

    log

--- middleware/trailingSlashRedirect.js
+++ middleware/trailingSlashRedirect.js
@@ -1,9 +1,9 @@
 export default function ({ route, redirect }) {
+  console.log(route);
   if (route.path !== "/" && route.path.endsWith("/")) {
     const { path, query, hash } = route;
     const nextPath = path.replace(/\/+$/, "") || "/";
     const nextRoute = { path: nextPath, query, hash };
-    console.log(nextRoute);
     redirect(nextRoute);
   }
 }
